64ad71c4694cf845760e10d9ad5935cc0cbf66bb
dekkerglen <dekker.glen@gmail.com>
2020-09-26 21:36:47 -0400

    fix issue where full energy is harvested during spawn time

--- spawning.js
+++ spawning.js
@@ -40,18 +40,21 @@ const buildOrder = [
 
 const spawnNext = (room, counts) => {
   if(room.energyAvailable >= room.energyCapacityAvailable) {
-    const spawn = room.find(FIND_MY_SPAWNS, {
+    const spawns = room.find(FIND_MY_SPAWNS, {
         filter: (object) => !object.spawning
-    })[0];
-    for(const phase of buildOrder) {
-        if(phase.condition(room) && (counts[phase.creep.role] || 0) < phase.amount) {
-          console.log(`trying to spawn ${phase.creep.role}`)
-            const res = phase.creep.build(spawn, room.energyAvailable);
-            if(res != 0) {
-              console.log(`Error spawning: ${res}`);
-            }
-            return;
-        }
+    });
+    if(spawns.length > 0) {
+      const spawn = spawns[0];
+      for(const phase of buildOrder) {
+          if(phase.condition(room) && (counts[phase.creep.role] || 0) < phase.amount) {
+            console.log(`trying to spawn ${phase.creep.role}`)
+              const res = phase.creep.build(spawn, room.energyAvailable);
+              if(res != 0) {
+                console.log(`Error spawning: ${res}`);
+              }
+              return;
+          }
+      }
     }
   }
 }
